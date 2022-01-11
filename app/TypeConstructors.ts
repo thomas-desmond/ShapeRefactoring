ca53566d05c9a0aaafc1736ba4191220c6201b65
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2022-01-11 07:26:30 -0800

    R Added Point class

--- app/TypeConstructors.ts
+++ app/TypeConstructors.ts
@@ -1,18 +1,21 @@
+class Point  {
+    type: string;
+    x: any;
+    y: any;
+
+    constructor(x?, y?) {
+        this.type = 'Point';
+        this.x = x ?? 0;
+        this.y = y ?? 0;
+    }
+}
+
 export function build(...coords: [number, number][]) {
     return inanimatus(coords)
 }
 
-export function asPoint(x, y): any {
-    let obj = {}
-    expectoPatronum(x, y, obj)
-    return obj
-}
-
-
-function expectoPatronum(x, y, obj) {
-    obj.type = 'Point'
-    obj.x = x
-    obj.y = y
+export function asPoint(x, y): Point {
+    return new Point(x,y)
 }
 
 function inanimatus(seating) {
